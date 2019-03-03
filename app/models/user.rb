6511d3a340705cfe0b7596c8ae3a02df41d6efa7
pam- <pam.assogba@gmail.com>
2019-03-03 18:16:02 -0500

    user model wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :received_appt_requests, class_name: 'Appointment', foreign_key: "coach_id"
+  has_many :sent_appt_requests, class_name: 'Appointment', foreign_key: "client_id"
 end

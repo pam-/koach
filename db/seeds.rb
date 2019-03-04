98ed0f636818c15f2cebafaa09e61be55a64079c
pam- <pam.assogba@gmail.com>
2019-03-04 10:23:21 -0500

    better puts messages for seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,27 @@
 require 'faker'
 require 'active_support/all'
 
-puts 'creating fake coaches and timeslots'
+puts 'Creating a user...'
 client = User.create(name: Faker::Name.name, is_coach: false)
+puts "User: #{client.inspect}"
+
+puts 'Creating coaches and timeslots...'
 5.times do |n|
   coach = User.create(name: Faker::Name.name, is_coach: true)
   timezone = ActiveSupport::TimeZone.us_zones.sample
 
   puts "coach ##{n}: #{coach.inspect}"
-  # TODO create optional MULTIPLE timeslots for a single day
   %w(monday tuesday wednesday thursday friday saturday sunday).each do |day|
     puts "coach ##{n}: #{coach.id}"
     next unless [true, false].sample
     start_time = Faker::Time.between(1.day.ago, 1.day.from_now, :morning).at_beginning_of_hour
     end_time = start_time.advance(hours: Faker::Number.between(2, 8), minutes: [0, 30].shuffle[0])
-    # save time_slot for each increment of 30mn
+    # save time_slot with increment of 30mn until we get to end of avalibility
     until start_time == end_time
-      puts "start time: #{start_time}"
-      TimeSlot.create(day: day, from: start_time, to: start_time, coach_id: coach.id, timezone: timezone.name, is_open: true)
+      timeslot = TimeSlot.create(day: day, from: start_time, to: start_time, coach_id: coach.id, timezone: timezone.name, is_open: true)
       start_time += 30.minutes
     end
   end
 end
+
+puts 'Done seeding data!'

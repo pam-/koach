331492da8218bc24b018087e7d88560bbdf54692
pam- <pam.assogba@gmail.com>
2019-03-04 11:03:17 -0500

    mixed up my foreign keys

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :received_appointments, class_name: 'Appointment', foreign_key: 'client_id'
-  has_many :booked_appointments, class_name: 'Appointment', foreign_key: 'coach_id'
+  has_many :received_appointments, class_name: 'Appointment', foreign_key: 'coach_id'
+  has_many :booked_appointments, class_name: 'Appointment', foreign_key: 'client_id'
   has_many :time_slots, foreign_key: 'coach_id'
 
   validates_presence_of :name

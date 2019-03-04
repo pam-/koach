7b5ee5091a23eafd4069d67d1348a88377f2da87
pam- <pam.assogba@gmail.com>
2019-03-03 21:35:59 -0500

    vue setup wip

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,25 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import Vue from 'vue';
+import axios from 'axios';
+
+import './styles/index';
+
+// Set a default CSRF token
+let token = document.getElementsByName('csrf-token')[0].getAttribute('content');
+axios.defaults.headers.common['X-CSRF-Token'] = token;
+axios.defaults.headers.common['Accept'] = 'application/json';
+window.axios = axios;
+
+import App from "./components/app.vue";
+
+// Declare a global component: modal
+// Vue.component('base-modal', require('./components/base-modal.vue').default);
+
+const app = new Vue({
+  el: '#app',
+  components: {
+    App
+  }
+});

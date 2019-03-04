45a4b776d9e64ffa6cf46836a81af38e95361f75
pam- <pam.assogba@gmail.com>
2019-03-03 21:35:03 -0500

    setting up helper methods for slots

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -1,4 +1,16 @@
 class TimeSlot < ApplicationRecord
   belongs_to :coach, class_name: 'User', foreign_key: 'coach_id'
   has_one :appointment
+
+  %w(monday tuesday wednesday thursday friday saturday sunday).each do |day|
+    scope day.to_sym, -> { where(:day => day) }
+  end
+
+  def formatted_from
+    from.strftime("%l:%M %p")
+  end
+
+  def formatted_to
+    to.strftime("%l:%M %p")
+  end
 end

d801fd1e09f52a06fab8e012241a3e7a58d36e1f
pam- <pam.assogba@gmail.com>
2019-03-03 23:44:01 -0500

    appt controller wip

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,6 +1,11 @@
 class AppointmentsController < ApplicationController
   def welcome
     @coaches = User.where(is_coach: true)
-    @time_slots = TimeSlot.all
+  end
+
+  def create
+    # create appointment with time slot
+    # edit time slot to false open
+    # redirect to user appointments or render modal with confirmation (gotta remove the newly booked appointment from list)
   end
 end

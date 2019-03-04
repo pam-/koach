e6a7785a4959498798e9e99410db36fec9ffe2d3
pam- <pam.assogba@gmail.com>
2019-03-04 09:35:47 -0500

    adding crearte action

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,11 +1,21 @@
 class AppointmentsController < ApplicationController
   def welcome
     @coaches = User.where(is_coach: true)
+    @appointment = Appointment.new
   end
 
   def create
-    # create appointment with time slot
-    # edit time slot to false open
-    # redirect to user appointments or render modal with confirmation (gotta remove the newly booked appointment from list)
+    @appointment = Appointment.new(appointment_params)
+    if @appointment.save_and_update_slot
+      redirect_to root_path, flash: { success: "Successfully booked the appointment!"}
+    else
+      redirect_to root_path, flash: { error: "We couldn't book the appointment. Please reload and try again!"}
+    end
+  end
+
+  protected
+
+  def appointment_params
+    params.require(:appointment).permit(:coach_id, :client_id, :time_slot_id, is_confirmed: false)
   end
 end

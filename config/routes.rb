3e8d326ad42e71159bba101911c86246af04ec72
pam- <pam.assogba@gmail.com>
2019-03-04 11:10:46 -0500

    adding new root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'appointments#welcome'
+  root to: 'time_slots#index'
 
   resources :appointments
   resources :users

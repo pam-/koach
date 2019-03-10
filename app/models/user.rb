d5b257f6e3bde541ca5b531dc2c4d0617f1f037e
pam- <pam.assogba@gmail.com>
2019-03-10 19:48:31 -0400

    adding user omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ApplicationRecord
   has_many :time_slots, foreign_key: 'coach_id'
 
   validates_presence_of :name
+  validates_uniqueness_of :email, scope: :name
   validates :is_coach, inclusion: [true, false]
+
+  def self.from_omniauth(auth_hash)
+    return where(name: auth_hash.info.name).first_or_create do |user|
+      user.name = auth_hash.info.name
+      user.email = auth_hash.info.email
+    end
+  end
 end

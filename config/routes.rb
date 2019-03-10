325ef9086467921e4575ffca3b88e6c8855b9d92
pam- <pam.assogba@gmail.com>
2019-03-10 19:49:27 -0400

    addingg routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'time_slots#index'
 
+  get '/auth/google_oauth2'
+  get '/auth/google_oauth2/callback', to: 'auth#omniauth'
+  get 'login', to: 'auth#login'
+  get 'logout', to: 'auth#logout'
+
   resources :appointments
   resources :users
 end

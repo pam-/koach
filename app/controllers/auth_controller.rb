2b22b7c816017931390b8fce05982be45f1c8c1e
pam- <pam.assogba@gmail.com>
2019-03-11 10:48:44 -0400

    removing trailing spaces

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,14 +1,14 @@
 class AuthController < ApplicationController
   def omniauth
     access_token = request.env["omniauth.auth"]
-    user = User.from_omniauth(access_token)  
+    user = User.from_omniauth(access_token)
 
     if user.present?
       @current_user = User.find_by(id: user.id)
       session[:user_id] = @current_user.id
       redirect_to root_path
     else
-      flash[:error] = "We couldn't log you in. Please try again." 
+      flash[:error] = "We couldn't log you in. Please try again."
       redirect_to login_path
     end
   end
